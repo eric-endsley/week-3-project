e8d3d362d8ab2e5b3dfa60438b5678620778694b
Eric Endsley <eric.endsley4@gmail.com>
2020-11-02 10:22:57 -0800

    save changes listed in previous commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,29 @@
-// UI Logic:
+
 $(document).ready(function() {
+
+// UI Logic:
   $("form#beep-boop").submit(function(event) {
     event.preventDefault();
 
     const numberInput = parseInt($("input#numberInput").val());
     $(".output").text(beepBoop(numberInput).toString());
-  
-
-
-
+  });
 
 // Business Logic: 
-    function beepBoop(inputNumber) {
-      let numberArray = [...Array(inputNumber+1).keys()];
-      let beepBoopArray = []
-      numberArray.forEach(function(number) {
-        if (number.toString().includes("3")) {
-          beepBoopArray.push(" Won't you be my neighbor?")
-        } else if (number.toString().includes("2")) {
-          beepBoopArray.push(" Boop!")
-        } else if (number.toString().includes("1")) {
-          beepBoopArray.push(" Beep!")
-        } else {
-          beepBoopArray.push(" " + number)
-        };
-      });
-      return(beepBoopArray)
-    };
-  });
+  function beepBoop(inputNumber) {
+    let numberArray = [...Array(inputNumber+1).keys()];
+    let beepBoopArray = []
+    numberArray.forEach(function(number) {
+      if (number.toString().includes("3")) {
+        beepBoopArray.push(" Won't you be my neighbor?")
+      } else if (number.toString().includes("2")) {
+        beepBoopArray.push(" Boop!")
+      } else if (number.toString().includes("1")) {
+        beepBoopArray.push(" Beep!")
+      } else {
+        beepBoopArray.push(" " + number)
+      };
+    });
+    return(beepBoopArray)
+  };
 });
