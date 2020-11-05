240ec5ae35d42ab33861ebe2a0acdbf96e0881be
Eric Endsley <eric.endsley4@gmail.com>
2020-11-04 22:30:56 -0800

    add jumbotron containing title and subheader, style jumbotron text to center

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,29 @@
 
 // Business Logic: 
-  function beepBoop(inputNumber) {
-    let numberArray = [...Array(inputNumber+1).keys()];
-    let beepBoopArray = []
-    numberArray.forEach(function(number) {
-      if (number.toString().includes("3")) {
-        beepBoopArray.push(" Won't you be my neighbor?")
-      } else if (number.toString().includes("2")) {
-        beepBoopArray.push(" Boop!")
-      } else if (number.toString().includes("1")) {
-        beepBoopArray.push(" Beep!")
-      } else {
-        beepBoopArray.push(" " + number)
-      };
-    });
-    return(beepBoopArray)
-  };
+function beepBoop(inputNumber) {
+  let numberArray = [...Array(inputNumber+1).keys()];
+  let beepBoopArray = []
+  numberArray.forEach(function(number) {
+    if (number.toString().includes("3")) {
+      beepBoopArray.push(" Won't you be my neighbor?")
+    } else if (number.toString().includes("2")) {
+      beepBoopArray.push(" Boop!")
+    } else if (number.toString().includes("1")) {
+      beepBoopArray.push(" Beep!")
+    } else {
+      beepBoopArray.push(" " + number)
+    };
+  });
+  return(beepBoopArray)
+};
 
   // UI Logic:
-  $(document).ready(function() {
-    $("form#beep-boop").submit(function(event) {
-      event.preventDefault();
-  
-      const numberInput = parseInt($("input#numberInput").val());
-      const outputArray = beepBoop(numberInput)
-      $(".output").text(outputArray);
-    });
-  
+$(document).ready(function() {
+  $("form#beep-boop").submit(function(event) {
+    event.preventDefault();
+
+    let numberInput = parseInt($("input#numberInput").val());
+    let outputArray = beepBoop(number-input)
+    $(".output").text(outputArray);
+  });
 });
